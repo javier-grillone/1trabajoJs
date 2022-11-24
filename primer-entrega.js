52ea1139d00868f96bb7f1dab642e96a8c2a65b1
Javier Grillone <javiermartinez_1998@hotmail.com>
2022-11-24 19:09:37 -0300

    condicional

---
+++ primer-entrega.js
@@ -0,0 +1,13 @@
+/* adgoritmo con un condicional */
+//si el consumidor es mayor puede comprar si es menor no
+// el consumidor tiene 25 entonces es mayor a 18 y el mensaje que se muestra en pantalla es puede realizar su compra
+
+//let edad = prompt ("ingrese su edad para poder realizar la compra del vape" )
+
+
+/* if (edad >= 18) {
+    alert( "puede comprar su vape")
+} else {
+    alert (  "no puede realizar su compra" )
+}
+ */ 
